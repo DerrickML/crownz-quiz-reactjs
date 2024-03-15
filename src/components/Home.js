b26cca9ef23663353a08164da238bbdc93084748
DeroMal <derrickmal123@gmail.com>
2024-03-15 13:10:44 +0300

    Implemented points fetching and update on login to display to user

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faUserCircle } from "@fortawesome/free-solid-svg-icons";
-import { Container, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Alert, Badge } from "react-bootstrap";
 import storageUtil from "../utilities/storageUtil";
 import { useAuth } from "../context/AuthContext";
 import "../animations.css";
@@ -14,37 +14,58 @@ import NextOfKinDashboard from "./dashboard/NextOfKinDashboard";
 
 function Home() {
   const navigate = useNavigate();
-  const { userInfo } = useAuth();
+  const { userInfo, userPoints, updateUserPoints, fetchUserPoints, } = useAuth();
   // const userInfo = storageUtil.getItem("userInfo");
   const isStudent = userInfo.labels.includes("student");
   const isNextOfKin = userInfo.labels.includes("kin");
 
   const renderHeroHeader = () => (
     <HeroHeader>
-      <h1 className="display-4">
-        Welcome to Your Dashboard, {userInfo.firstName}!
-      </h1>
-      <p className="lead">
-        {isStudent
-          ? "Explore your academic journey, track progress, and excel in your studies."
-          : "Stay informed about your child's academic journey, track their progress, and support their success."}
-      </p>
-      <div className="d-flex justify-content-center mt-4">
-        <Button
-          variant="light"
-          className="me-2"
-          onClick={() => navigate("/profile")}
-        >
-          <FontAwesomeIcon icon={faUserCircle} className="me-2" />
-          View Profile
-        </Button>
-        {isStudent && (
-          <Button variant="secondary" onClick={() => navigate("/exam-page")}>
-            <FontAwesomeIcon icon={faEdit} className="me-2" />
-            Attempt Exam
-          </Button>
-        )}
-      </div>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md={8}>
+            <h1 className="display-4">
+              Welcome to Your Dashboard, {userInfo.firstName}!
+            </h1>
+            <p className="lead">
+              {isStudent
+                ? "Explore your academic journey, track progress, and excel in your studies."
+                : "Stay informed about your child's academic journey, track their progress, and support their success."}
+            </p>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center mt-4">
+          <Col md="auto">
+            <Button variant="light" onClick={() => navigate("/profile")}>
+              <FontAwesomeIcon icon={faUserCircle} /> View Profile
+            </Button>
+          </Col>
+          {isStudent && (
+            <Col md="auto">
+              <Button variant="secondary" onClick={() => navigate("/exam-page")}>
+                <FontAwesomeIcon icon={faEdit} /> Attempt Exam
+              </Button>
+            </Col>
+          )}
+        </Row>
+        <Row className="justify-content-md-center mt-4">
+          <Col md={8}>
+            <Alert variant="primary" className="d-flex align-items-center">
+              <div className="flex-grow-1">
+                Points Left: <Badge bg="dark">{userPoints}</Badge>
+              </div>
+              <div>
+                <Button variant="success" onClick={() => fetchUserPoints(userInfo.userId, userInfo.educationLevel)}>
+                  <FontAwesomeIcon icon={faEdit} /> Fetching points
+                </Button>
+                <Button variant="warning" onClick={() => updateUserPoints(20, userInfo.userId)}>
+                  <FontAwesomeIcon icon={faEdit} /> Attempt Exam
+                </Button>
+              </div>
+            </Alert>
+          </Col>
+        </Row>
+      </Container>
     </HeroHeader>
   );
 
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,22 @@
 // AuthContext.js
-import React, { createContext, useState, useContext } from 'react';
-import { account } from "../appwriteConfig.js";
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import {
+    client,
+    account,
+    databases,
+    database_id,
+    studentTable_id,
+    nextOfKinTable_id,
+    studentMarksTable_id,
+    sstTablePLE_id,
+    couponTable_id,
+    transactionTable_id,
+    pointsTable_id,
+    pointsBatchTable_id,
+    Permission,
+    Role,
+    Query,
+} from "../appwriteConfig.js";
 import storageUtil from '../utilities/storageUtil.js';
 
 const AuthContext = createContext(null);
@@ -8,15 +24,9 @@ const AuthContext = createContext(null);
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-    const [sessionInfo, setSessionInfo] = useState(() => {
-        // Get the session info from storage or return null
-        return storageUtil.getItem("sessionInfo");
-    });
-
-    const [userInfo, setUserInfo] = useState(() => {
-        // Get the user info from storage or return null
-        return storageUtil.getItem("userInfo");
-    });
+    const [sessionInfo, setSessionInfo] = useState(storageUtil.getItem("sessionInfo"));
+    const [userInfo, setUserInfo] = useState(storageUtil.getItem("userInfo"));
+    const [userPoints, setUserPoints] = useState(storageUtil.getItem("userPoints") || 0);
 
     const handleLogin = async (sessionData, userData) => {
         const sessionDetails = {
@@ -50,6 +60,12 @@ export const AuthProvider = ({ children }) => {
 
         setUserInfo(userDetails);
         storageUtil.setItem("userInfo", userDetails);
+
+        // Fetch userPoints from the database after login
+        // This is an example, replace it with your actual API call
+        const pointsData = await fetchUserPoints(userData.userId, userData.educationLevel);
+        setUserPoints(pointsData);
+        storageUtil.setItem("userPoints", pointsData);
     };
 
     const handleLogout = async () => {
@@ -66,10 +82,95 @@ export const AuthProvider = ({ children }) => {
         setSessionInfo(null);
         setUserInfo(null);
         storageUtil.clear();
+
+        // Clear userPoints from context and storage
+        setUserPoints(0);
+        storageUtil.removeItem("userPoints");
+    };
+
+    // Update userPoints in local storage and database
+    const updateUserPoints = async (PointsToDeduct, userId) => {
+        // Update points in the database and then in the context and storage
+        await saveUserPointsToDatabase(PointsToDeduct, userId);
+
+        // const updatedPoints = await saveUserPointsToDatabase(PointsToDeduct, userId);
+        // setUserPoints(updatedPoints);
+        // storageUtil.setItem("userPoints", updatedPoints);
+    };
+
+    // Fetch userPoints function (example)
+    const fetchUserPoints = async (userId, educationLevel) => {
+        try {
+            let pointsData
+            const pointsResponse = await databases.listDocuments(database_id, pointsTable_id, [Query.equal('UserID', userId)])
+            //Create a new document if user has no document assigned
+            if (pointsResponse.documents.length === 0) {
+                console.log('No user document assigned, creating a new one for the user')
+                const userDocResponse = await databases.createDocument(
+                    database_id,
+                    pointsTable_id,
+                    "unique()",
+                    {
+                        UserID: userId || null,
+                        PurchasedTier: educationLevel,
+                        AcquisitionDate: new Date().toLocaleString(),
+                        ExpiryDate: new Date().toLocaleString(),
+                    }
+                );
+                pointsData = 0
+            }
+
+            pointsData = pointsResponse.documents[0].PointsBalance
+
+
+            setUserPoints(pointsData);
+            storageUtil.setItem("userPoints", pointsData);
+        } catch (error) {
+            console.error("Error fetching user points:", error);
+        }
+    };
+
+    // Save userPoints function (example)
+    const saveUserPointsToDatabase = async (points, userId) => {
+        // Update points in the database
+        try {
+            let updatedPoints
+            const response = await databases.listDocuments(database_id, pointsTable_id, [
+                Query.equal("UserID", userId),
+            ]);
+            console.log('Checking points table: ', response)
+            if (response.documents.length > 0) { //TODO: If table user points doesn't exist, create new document
+                const documentId = response.documents[0].$id //Points document id to be updated
+                let currentPoints = response.documents[0].PointsBalance
+                updatedPoints = currentPoints - points
+                console.log('points document id: ', documentId)
+
+                //update Points table
+                const updateResponse = await databases.updateDocument(database_id, pointsTable_id, documentId, { PointsBalance: updatedPoints })
+                console.log('update points balance: ', updateResponse)
+
+                // Update points in context and localStorage
+                setUserPoints(updatedPoints);
+                storageUtil.setItem("userPoints", updatedPoints);
+            }
+
+            return updatedPoints;
+
+        } catch (error) {
+            console.error("Error updating user points:", error);
+        }
     };
 
     return (
-        <AuthContext.Provider value={{ sessionInfo, userInfo, handleLogin, handleLogout }}>
+        <AuthContext.Provider value={{
+            sessionInfo,
+            userInfo,
+            userPoints,
+            handleLogin,
+            handleLogout,
+            fetchUserPoints,
+            updateUserPoints,
+        }}>
             {children}
         </AuthContext.Provider>
     );
