2dbf96b8dba94e764190b0e18b7ea865afdb94b0
DeroMal <derrickmal123@gmail.com>
2024-04-08 19:30:19 +0200

    changed sandbox server url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 /** SERVER-SIDE URL **/
 // const serverUrl = 'http://localhost:3005'; //Localhost
-const serverUrl = 'https://h8s73r-3000.csb.app' //Derrick codesandbox
-// const serverUrl = 'https://nk7rt3-3005.csb.app' //Crownzcom codesandbox
+// const serverUrl = 'https://jz8w6z-3005.csb.app' //Derrick codesandbox
+const serverUrl = 'https://nk7rt3-3005.csb.app' //Crownzcom codesandbox
 
 /** Extract the root URL (protocol + hostname + port) **/
 const rootUrl = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
