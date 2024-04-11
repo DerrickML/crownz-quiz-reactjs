dd69dad2c7032c54dcee037be32e914d1084b158
DeroMal <derrickmal123@gmail.com>
2024-04-11 11:25:44 +0300

    - Added rootUrl as serverUrl option

--- src/config.js
+++ src/config.js
@@ -1,10 +1,11 @@
+/** Extract the root URL (protocol + hostname + port) **/
+const rootUrl = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
+
 /** SERVER-SIDE URL **/
 const serverUrl = 'http://localhost:3005'; //Localhost
 // const serverUrl = 'https://jz8w6z-3005.csb.app' //Derrick codesandbox
 // const serverUrl = 'https://nk7rt3-3005.csb.app' //Crownzcom codesandbox
-
-/** Extract the root URL (protocol + hostname + port) **/
-const rootUrl = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
+// const serverUrl = rootUrl; // Same server as the backend server-side
 
 export { serverUrl, rootUrl };
 
