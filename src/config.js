a752ef080c31da75ed15e66ee4f9bac2bd55a2b3
DeroMal <derrickmal123@gmail.com>
2024-07-15 11:41:44 +0200

    - changed to examprep server-side code - add directory to hold historical builds

--- src/config.js
+++ src/config.js
@@ -8,8 +8,8 @@ const rootUrl = window.location.protocol + '//' + window.location.hostname + (wi
 // const serverUrl = 'http://localhost:3005'; //Localhost
 
 // const serverUrl = 'https://jz8w6z-3005.csb.app' //Derrick codesandbox
-const serverUrl = 'https://9rtmcd-3005.csb.app' //Derrick123 codesandbox
-// const serverUrl = 'https://nk7rt3-3005.csb.app' //Crownzcom codesandbox
+// const serverUrl = 'https://9rtmcd-3005.csb.app' //Derrick123 codesandbox
+const serverUrl = 'https://nk7rt3-3005.csb.app' //Crownzcom codesandbox
 // const serverUrl = rootUrl; // Same server as the backend server-side
 
 export { serverUrl, rootUrl };
